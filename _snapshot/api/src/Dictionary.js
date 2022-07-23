902f0b168d5912d04e2dd6a04a844064a786965f
Elegangit <elegantbest20@gmail.com>
2022-07-23 15:26:37 -0400

    edited

---
+++ _snapshot/api/src/Dictionary.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
